d966fc0149f904936b34a7a670125772a918a3f0
baguskuncoro <baguskuncoro003@gmail.com>
2023-12-16 09:02:55 +0700

    feat: register

---
+++ src/pages/auth/register.tsx
@@ -0,0 +1,10 @@
+import RegisterView from '@/components/views/auth/Register';
+import React from 'react';
+
+export default function Register() {
+  return (
+    <>
+      <RegisterView />
+    </>
+  );
+}
